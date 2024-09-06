10a0ac3487a8a2224574e0d1e8969b5d54bfd183
Achaudhari02 <ashutoshc018@gmail.com>
2024-09-06 17:10:23 -0400

    created an endpoint for group user addition

--- backend/robson_insight/users/urls.py
+++ backend/robson_insight/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:pk>/', UserProfileDetail.as_view()),
     path('groups/', GroupListCreate.as_view()),
     path('groups/<int:pk>/', GroupListCreate.as_view()),
+    path('add-user-to-group/', AddUserToGroupView.as_view(), name='add-user-to-group'),
 ]
--- backend/robson_insight/users/views.py
+++ backend/robson_insight/users/views.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken.views import ObtainAuthToken, APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserProfileSerializer, GroupSerializer
 from .models import UserProfile, Group
+from django.contrib.auth.models import User
 
 
 class UserProfileList(generics.ListAPIView):
@@ -52,3 +53,31 @@ class GroupListCreate(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save()
+        
+class AddUserToGroupView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        username = request.data.get('username')
+        group_id = request.data.get('group_id')
+
+        if not username or not group_id:
+            return Response({'error': 'Username and group_id are required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.get(username=username)
+            group = Group.objects.get(id=group_id)
+            user_profile, created = UserProfile.objects.get_or_create(user=user)
+            user_profile.group = group
+            user_profile.save()
+            
+            return Response({'success': f'User {username} added to group {group.name}'}, status=status.HTTP_200_OK)
+        
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except User.DoesNotExist:
+            return Response({'error': 'Group not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+            
