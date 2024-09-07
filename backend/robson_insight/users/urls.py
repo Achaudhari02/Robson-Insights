83dd198e711f0585f0c27432cc013a86e89ee32d
Achaudhari02 <ashutoshc018@gmail.com>
2024-09-07 11:29:31 -0400

    added endpoint for removing user from a group

--- backend/robson_insight/users/urls.py
+++ backend/robson_insight/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('groups/', GroupListCreate.as_view()),
     path('add-user-to-group/', AddUserToGroupView.as_view(), name='add-user-to-group'),
     path('groups/<int:pk>/', GroupDetail.as_view()),
+    path('remove-user-from-group/', RemoveUserFromGroup.as_view(), name='remove-user-from-group'),
 ]
