89930e633546c99645635da4b725cf00bb39150c
Achaudhari02 <ashutoshc018@gmail.com>
2024-10-20 19:13:48 -0400

    Update tests.py in users with new tests

--- backend/robson_insight/users/tests.py
+++ backend/robson_insight/users/tests.py
@@ -82,3 +82,63 @@ class TogglePermissionsViewTests(APITestCase):
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertIn('error', response.data)
+
+class RemoveUserFromGroupTests(APITestCase):
+
+    def setUp(self):
+        self.admin_user = User.objects.create_user(username='admin', password='adminpass')
+        self.member_user = User.objects.create_user(username='member', password='memberpass')
+        self.other_user = User.objects.create_user(username='other', password='otherpass')
+
+        self.group = Group.objects.create(name='Test Group')
+
+        UserProfile.objects.create(user=self.admin_user, group=self.group, is_admin=True)
+        UserProfile.objects.create(user=self.member_user, group=self.group, is_admin=False)
+
+        self.admin_token = self.client.post('/login/', {'username': 'admin', 'password': 'adminpass'}).data['token']
+        self.member_token = self.client.post('/login/', {'username': 'member', 'password': 'memberpass'}).data['token']
+        self.other_token = self.client.post('/login/', {'username': 'other', 'password': 'otherpass'}).data['token']
+
+        self.url = reverse('users:remove-user-from-group')
+
+    def test_admin_can_remove_member(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token)
+        data = {
+            'username': 'member',
+            'group_id': self.group.id
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('success', response.data)
+
+        self.assertFalse(UserProfile.objects.filter(user=self.member_user, group=self.group).exists())
+
+    def test_non_admin_cannot_remove_member(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.member_token)
+        data = {
+            'username': 'other',
+            'group_id': self.group.id
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIn('error', response.data)
+
+    def test_remove_user_not_in_group(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token)
+        data = {
+            'username': 'other',
+            'group_id': self.group.id
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('error', response.data)
+
+    def test_remove_user_from_nonexistent_group(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token)
+        data = {
+            'username': 'member',
+            'group_id': 9999  
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('error', response.data)
